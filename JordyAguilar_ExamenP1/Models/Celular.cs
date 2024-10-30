8b398303278caa59598e6da2e33f809aa6a3334f
Jordy-Aguilar <jeay022@gmail.com>
2024-10-30 08:55:04 -0500

    Actualice clase Celular

--- JordyAguilar_ExamenP1/Models/Celular.cs
+++ JordyAguilar_ExamenP1/Models/Celular.cs
@@ -1,12 +1,17 @@
-﻿namespace JordyAguilar_ExamenP1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JordyAguilar_ExamenP1.Models
 {
     public class Celular
     {
+        
+        [Key]
         public int Id { get; set; }
-        public string Modelo {  get; set; }
-        public int Año { get; set; }
-        public float Precio { get; set; }
-
+        [StringLength(20)]
+        public string Modelo { get; set; }
+        [Range(2000, 2024)]
+        public int anio { get; set; }
+        public float precio { get; set; }
 
     }
 }
