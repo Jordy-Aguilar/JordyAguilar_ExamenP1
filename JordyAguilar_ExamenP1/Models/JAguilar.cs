2cbd24ae2e3376983fca809c0661555a6d2a9ba9
Jordy-Aguilar <jeay022@gmail.com>
2024-10-30 08:48:05 -0500

    Completar atributos de clase Celular

--- JordyAguilar_ExamenP1/Models/JAguilar.cs
+++ JordyAguilar_ExamenP1/Models/JAguilar.cs
@@ -1,9 +1,14 @@
-﻿namespace JordyAguilar_ExamenP1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JordyAguilar_ExamenP1.Models
 {
     public class JAguilar
     {
-        public String nombre {  get; set; }
+        public String Nombre {  get; set; }
         public float Salario {  get; set; }
-        public int 
+        public int Id { get; set; }
+        public String Descripcion { get; set; }
+        public bool TieneHijos { get; set; }
+        public DataType FechaNacimiento { get; set; }
     }
 }
