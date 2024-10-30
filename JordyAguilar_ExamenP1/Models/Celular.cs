b8c1beb63b4398545f893e2c009fcf856f207d0a
Jordy-Aguilar <jeay022@gmail.com>
2024-10-30 08:43:45 -0500

    Cree clase Celular con sus atributos

---
+++ JordyAguilar_ExamenP1/Models/Celular.cs
@@ -0,0 +1,12 @@
+﻿namespace JordyAguilar_ExamenP1.Models
+{
+    public class Celular
+    {
+        public int Id { get; set; }
+        public string Modelo {  get; set; }
+        public int Año { get; set; }
+        public float Precio { get; set; }
+
+
+    }
+}
---
+++ JordyAguilar_ExamenP1/Models/JAguilar.cs
@@ -0,0 +1,9 @@
+﻿namespace JordyAguilar_ExamenP1.Models
+{
+    public class JAguilar
+    {
+        public String nombre {  get; set; }
+        public float Salario {  get; set; }
+        public int 
+    }
+}
