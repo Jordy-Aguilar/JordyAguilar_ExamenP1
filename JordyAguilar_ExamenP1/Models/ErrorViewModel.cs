80eb5a49f3858154dd07325bb6b7c50805c5dd3d
Jordy-Aguilar <jeay022@gmail.com>
2024-10-30 08:33:47 -0500

    Agregar archivos de proyecto.

---
+++ JordyAguilar_ExamenP1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JordyAguilar_ExamenP1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
