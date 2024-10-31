9b51e937d6800ae1bddea3ea59d67405e681a674
Jordy Aguilar <jeay022@gmail.com>
2024-10-30 20:04:01 -0500

    Ultima actualizacion

--- JordyAguilar_ExamenP1/wwwroot/js/site.js
+++ JordyAguilar_ExamenP1/wwwroot/js/site.js
@@ -1,4 +1,4 @@
 ﻿
 function mostrarMensaje() {
-    alert("Hola soy Jordy Aguilar, tengo 20 años y me gusta mucho el futbol, tanto verlo como practicarlo, en especial me gusta el futbol europeo, La Liga, Premier, Bundesliga y sobre todo la Champions League");
+    alert("Hola soy Jordy Aguilar, tengo 20 años y me gusta mucho el futbol, tanto verlo como practicarlo, en especial me gusta el futbol europeo, La Liga, Premier, Bundesliga y sobre todo la Champions League, aaa y soy bicholover siuuu");
 }
